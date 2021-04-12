03e414f1e3ffe4882926a578601bd33d33426d6c
olivia-gambitsis <o.gambitsis@gmail.com>
2021-04-12 20:41:01 +1200

    add server

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 import WelcomeScreen from './app/screens/WelcomeScreen'
+import Browse from './app/screens/Browse'
 
 //SafeAreaView add padding on the top to ensure text is within safe area
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return(
-      <WelcomeScreen/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options = {{title:'Welcome'}}/>
+        <Stack.Screen
+          name="Browse"
+          component={Browse}
+          options = {{title:'Browse'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ express-server/routes/recipies.js
@@ -0,0 +1,71 @@
+const router = require('express').Router()
+const axios = require('axios')
+
+// define the default route that fetches all of our recipies
+router.get('/',(req, res) => {
+
+    // data the conserves our API quota for development
+    const placeholderData = [
+        {
+            "_id": "database1591127768852",
+            "recipie": "Hello",
+            "_createdOn": "2020-06-02T19:56:08.852Z",
+            "_lastModifiedOn": "2020-06-02T19:56:08.852Z"
+        },
+        {
+            "_id": "database1591134992139",
+            "recipie": "New recipie",
+            "_createdOn": "2020-06-02T21:56:32.139Z",
+            "_lastModifiedOn": "2020-06-02T21:56:32.139Z"
+        }
+    ]
+
+    try {
+        // add api call
+
+        res.json({ recipies: placeholderData })
+    } catch (e) {
+        console.log(e)
+        res.status(500).send('Error.')
+    }
+})
+
+router.post('/add',(req, res) => {
+    // extract recipie text from request body
+    const { recipie } = req.body
+
+    const data = {
+        recipie
+    }
+
+    console.log(recipie)
+
+    try {
+        // add api call
+
+        res.json({
+            message: 'recipie added'
+        })
+    } catch (e) {
+        console.log(e)
+        res.status(500).send("Error.")
+    }
+})
+
+router.post('/delete', (req, res) => {
+    // extract the recipie id to delete from request body
+    const { recipieId } = req.body
+
+    console.log(recipieId)
+
+    try {
+        // add api call
+
+        res.send('recipie deleted')
+    } catch (e) {
+        console.log(e)
+        res.status(500).send('Error.')
+    }
+})
+
+module.exports = router
